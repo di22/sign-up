8d2b090a66fe75b9224e2a0996d736b320323b2d
di22 <36845367+di22@users.noreply.github.com>
2023-11-19 19:36:21 +0100

    fix: get controls current values

--- src/app/shared/validations/form/custom-validators/password-validation.ts
+++ src/app/shared/validations/form/custom-validators/password-validation.ts
@@ -7,8 +7,8 @@ export const passwordStrengthValidator = (): ValidatorFn => {
 
     if (!value) return null;
 
-    const firstname: string = control.parent?.value?.firstName;
-    const lastname: string = control.parent?.value?.lastName;
+    const firstname: string = control.parent?.get('firstName')?.value;
+    const lastname: string = control.parent?.get('lastName')?.value;
 
     const hasUpperCase: boolean = /[A-Z]+/.test(value);
     const hasLowerCase: boolean = /[a-z]+/.test(value);
